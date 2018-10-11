9124473aee24ec0acc54ab178ff24a3aaa9fdf02
Alex Chaffee <alex@stinky.com>
2018-10-11 10:48:37 -0400

    add failing test for automatic rendering

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -19,15 +19,18 @@ describe('The Page', function () {
   });
 
   it('has a source text field', function () {
-    cy.get('textarea#source').should('be.visible');
+    cy.get('textarea#source')
+      .should('be.visible');
   });
 
   it('has a preview area', function () {
-    cy.get('div#preview').should('be.visible');
+    cy.get('div#preview')
+      .should('be.visible');
   });
 
   it('has a "render" button', function () {
-    cy.get('button#render').should('be.visible');
+    cy.get('button#render')
+      .should('be.visible');
   });
 });
 
@@ -36,7 +39,7 @@ describe('The Render button', function () {
     cy.get('textarea#source').type('# Hello');
     cy.get('button').click();
     cy.get('div#preview').should('be.visible');
-    cy.get('div#preview').then((el)=> {
+    cy.get('div#preview').then((el) => {
       assert.include(el.text(), 'Hello');
     });
     console.log(cy.get('div#preview'));
@@ -46,9 +49,24 @@ describe('The Render button', function () {
     cy.get('textarea#source').clear();
     cy.get('textarea#source').type('# Hello\n* bullet');
     cy.get('button').click();
-    cy.get('div#preview').within(function () {
+    cy.get('div#preview').within(($div) => {
       cy.get('h1').should('have.text', 'Hello');
       cy.get('li').should('have.text', 'bullet');
+    });
+  });
+});
+
+describe('Automatic Rendering', function() {
+  it('happens eventually', function() {
+    cy.get('textarea#source').clear();
+
+    cy.get('div#preview').invoke('empty'); // https://api.jquery.com/empty/
+    cy.get('div#preview h1').should('not.have.text', 'Hello');
+
+    cy.get('textarea#source').type('# Hello\n* bullet');
+    cy.get('div#preview').within(($div) => {
+      cy.get('div#preview h1').should('have.text', 'Hello');
+      cy.get('div#preview li').should('have.text', 'bullet');
     });
   });
 });
